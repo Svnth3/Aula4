83bcf3a1690b529b25186962dc7682f7abe58121
Svnth3 <marinsfraga@gmail.com>
2025-04-15 19:37:43 -0300

    Atualização

--- aula4/exmplo1.py
+++ aula4/exmplo1.py
@@ -1,7 +1,7 @@
 valor_compra = float(input("Insira o valor da compra: "))
 valor_desconto = float(0.16)
-
-if valor_compra > 250:
+LIMITE = 250 #iLetra maiscula é utilizada para constante ou seja valores que não mudam 
+if valor_compra > LIMITE:
     valor_compra = valor_compra - (valor_compra * valor_desconto)
     print(f'O valor será de R${valor_compra:.2f}')
 else:
